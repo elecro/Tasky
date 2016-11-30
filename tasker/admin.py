60becd7e83788616314b09d5f17e30f9ceb8848a
Peter Gal <galpeter@inf.u-szeged.hu>
2016-11-30 19:44:46 +0100

    Add tasker models

--- tasker/admin.py
+++ tasker/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Task, TaskState
 # Register your models here.
+
+@admin.register(TaskState)
+class TaskStateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
--- tasker/models.py
+++ tasker/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TaskState(models.Model):
+    name = models.CharField(max_length=50)
+
+class Task(models.Model):
+    owner = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    state = models.ForeignKey(TaskState)
